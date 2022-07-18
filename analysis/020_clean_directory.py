7e2d5894598db1c9cb01456fe5d5df19d2aff2d8
Jonas Hund <jonas.hund@ntnu.no>
2022-07-18 10:46:13 +0200

    refine clean directory script

--- analysis/020_clean_directory.py
+++ analysis/020_clean_directory.py
@@ -4,3 +4,20 @@ import pathlib
 
 # remove created directories
 shutil.rmtree(pathlib.Path(os.path.dirname(__file__), "expAna_plots"))
+
+os.remove(
+    pathlib.Path(
+        os.path.dirname(__file__),
+        "expAna_data",
+        "analysis_pcAbs_4555_uniax_tension_stress_crosshead_speed_0.1_specimen_orientation.pickle",
+    )
+)
+
+
+os.remove(
+    pathlib.Path(
+        os.path.dirname(__file__),
+        "expAna_data",
+        "analysis_pcAbs_4555_uniax_tension_vol_strain_specimen_orientation_parallel_to_flow_crosshead_speed.pickle",
+    )
+)
